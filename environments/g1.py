144323fc637ec718f6a1878893f94cd723f1e8df
Sergei Surovtsev <97428129+stillonearth@users.noreply.github.com>
2022-09-16 01:10:41 +0300

    updated parameters for episode cancel

--- environments/g1.py
+++ environments/g1.py
@@ -30,7 +30,7 @@ class G1DistanceEnv(MujocoEnv, utils.EzPickle):
         contact_cost_weight=5e-4,
         healthy_reward=1.0,
         terminate_when_unhealthy=True,
-        healthy_z_range=(0.057, 5.0),
+        healthy_z_range=(0.1, 1.0),
         contact_force_range=(-1.0, 1.0),
         reset_noise_scale=0.1,
         exclude_current_positions_from_observation=True,
